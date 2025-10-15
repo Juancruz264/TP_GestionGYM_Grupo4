6fc933bd0748309544509133f7f109620837348d
ainaraparedes <ainaraparedes.escuela@gmail.com>
2025-10-15 20:34:48 -0300

    masClases
    agrego el DbContext, no llegue a hacer las migraciones!! ;(

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using clasesGYM_;
+public class AplicationDbContext: DbContext
+{
+    public DbSet<Factura> Facturas { get; set; }
+    public DbSet<Pago> Pagos { get; set; }
+    public DbSet<Plan> Planes { get; set; }
+    public DbSet<Suscripcion> Suscripciones { get; set; }   
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Horario> Horarios { get; set; }
+    public DbSet<Clases> Clases { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+    "Server=localHost;Database=SistemaGym;Trusted_Connection=True;TrustServerCertificate=True;"
+);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Factura>().ToTable("Facturas");
+        modelBuilder.Entity<Pago>().ToTable("Pagos");
+        modelBuilder.Entity<Plan>().ToTable("Planes");
+        modelBuilder.Entity<Suscripcion>().ToTable("Suscripciones");
+        modelBuilder.Entity<Cliente>().ToTable("Clientes");
+        modelBuilder.Entity<Horario>().ToTable("Horarios");
+        modelBuilder.Entity<Clases>().ToTable("Clases");
+
+
+    }
+
+}
